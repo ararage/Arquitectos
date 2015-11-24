b7f602752c3e76d51a31c0204f9d101c6523453f
fuce <ferefuc@gmail.com>
2015-11-24 17:06:05 -0600

    Initializing project

---
+++ app/Events/Event.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Arquitectos\Events;
+
+abstract class Event
+{
+    //
+}
---
+++ app/Http/Requests/Request.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Arquitectos\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+abstract class Request extends FormRequest
+{
+    //
+}
